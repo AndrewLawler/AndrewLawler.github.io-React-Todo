d714bbbdb6e9c9929567a0607600e70df62807eb
AndrewLawler <AndrewLawler_@hotmail.com>
2019-03-31 15:18:48 +0100

    Updates

---
+++ precache-manifest.e0e940113d3290db3792b854675a50aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fab73d75525f9d6064a",
+    "url": "/Todo/static/js/runtime~main.ee49a031.js"
+  },
+  {
+    "revision": "abc4841b5e12e8eb225c",
+    "url": "/Todo/static/js/main.a3f3c183.chunk.js"
+  },
+  {
+    "revision": "4400df62da4bb8ec7ee9",
+    "url": "/Todo/static/js/2.322a2ec6.chunk.js"
+  },
+  {
+    "revision": "abc4841b5e12e8eb225c",
+    "url": "/Todo/static/css/main.e6f84ab8.chunk.css"
+  },
+  {
+    "revision": "655390d489c527b1cce7a4d7f101f7d6",
+    "url": "/Todo/index.html"
+  }
+];
